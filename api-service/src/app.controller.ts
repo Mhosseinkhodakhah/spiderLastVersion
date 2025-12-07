c959e4f2cb772b417d8ff2b163f5141a8f139f4f
hossein <hosseinkhodakhah6@gmail.com>
2025-12-07 12:33:56 +0330

    dev ::: add gaurd

--- api-service/src/app.controller.ts
+++ api-service/src/app.controller.ts
@@ -2,7 +2,7 @@ import { Body, Controller, Get, Param, Post, Query, UseGuards, ValidationPipe } 
 import { AppService } from './app.service';
 import { loginDto, registerDto } from './dto/login.dto';
 import { timeFrameType } from './dto/types';
-import { AuthGuard } from './auth/auth.guard';
+import { JwtAuthGuard } from './auth/jwt-auth.guard';
 
 @Controller()
 export class AppController {
@@ -10,53 +10,53 @@ export class AppController {
 
 
   @Get('balances')
-  @UseGuards(AuthGuard)
+  @UseGuards(JwtAuthGuard)
   getBalances(): {} {
     return this.appService.getBalances();
   }
 
 
   @Get('price')
-  // @UseGuards(AuthGuard)
+  // @UseGuards(JwtAuthGuard)
   getPrice(@Query('currency') currency : string , @Query('timeFrame') timeFrame : any): {} {
     return this.appService.getPrice(currency , timeFrame);
   }
 
 
   @Get('transactionlist')
-  @UseGuards(AuthGuard)
+  @UseGuards(JwtAuthGuard)
   getTransActions(): {} {
     return this.appService.getTransActions();
   }
 
 
   @Get('/market')
-  // @UseGuards(AuthGuard)
+  // @UseGuards(JwtAuthGuard)
   getState(): {} {
     return this.appService.getState();
   }
 
 
   @Post('/market')
-  // @UseGuards(AuthGuard)
+  // @UseGuards(JwtAuthGuard)
   setState(@Query('state') state : number , @Query('rsi') rsi : string , @Query('lastPrice') lastPrice : string , @Query('lastSellPrice') lastSellPrice : string , @Query('lastBuyPrice') lastBuyPrice : string , @Query('lastState') lastState : number): {} {
     return this.appService.setState(state , rsi , lastPrice , lastState ,lastSellPrice , lastBuyPrice);
   }
 
   @Post('login')
-  @UseGuards(AuthGuard)
+  @UseGuards(JwtAuthGuard)
   async login(@Body() body : loginDto){
     return this.appService.login(body)
   }
 
   @Post('register')
-  @UseGuards(AuthGuard)
+  @UseGuards(JwtAuthGuard)
   async registre(@Body(new ValidationPipe()) body : registerDto){
     return this.appService.register(body)
   }
 
   @Post('state')
-  // @UseGuards(AuthGuard)
+  // @UseGuards(JwtAuthGuard)
   async setStateOfMarket(@Body() body : loginDto){
     return this.appService.login(body)
   }
---
+++ api-service/src/auth/jwt.strategy.ts
@@ -0,0 +1,24 @@
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable } from '@nestjs/common';
+// import { UserService } from '../user/user.service';
+
+
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy, "jwt") {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: 'asdfghgfdsasdfgfdsasdfghgfdsasdfghjhgfdsdfghuyewqwertytrewqwertytrewq123648529852352638413841536354',
+    });
+  }
+
+  async validate(payload: any) {
+    // const user = await this.usersService.findOne(payload.phone);
+    console.log('its payload of the token >>>> ' , payload)
+    return payload
+  }  
+}
+
