48d9b63db6be2c6911451c97ead0a365d6d3d3aa
hossein <hosseinkhodakhah6@gmail.com>
2025-12-07 12:20:38 +0330

    dev ::: add gaurd

--- api-service/src/auth/auth.guard.ts
+++ api-service/src/auth/auth.guard.ts
@@ -8,8 +8,11 @@ export class AuthGuard implements CanActivate {
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
+    console.log('its hereeeeee')
     const request = context.switchToHttp().getRequest();
+    console.log('its hereeeeee' , request)
     const apiKey = request.headers;
+    console.log('its hereeeeee' , apiKey.Authorization)
     if (apiKey && apiKey.Authorization) {
       try {
         let decode = this.jwt.verify(apiKey.Authorization)
