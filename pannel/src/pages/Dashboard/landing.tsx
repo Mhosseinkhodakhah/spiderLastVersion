04c42236949389c968bf8301e858e3a082c8808c
hossein <hosseinkhodakhah6@gmail.com>
2025-12-23 16:20:52 +0330

    adsf

--- pannel/src/pages/Dashboard/landing.tsx
+++ pannel/src/pages/Dashboard/landing.tsx
@@ -205,7 +205,7 @@ export default function LandingPage() {
                     
                     {/* ایموجی را حذف کرده و برای استفاده از تصویر خود، زیر را آنکامنت کنید */}
                     <img 
-                      src={'/itslogo.jpeg'} 
+                      src={'/itslogo.jpg'} 
                       alt="ربات معاملاتی اسپایدر" 
                       className="w-full h-full object-contain"
                     />
